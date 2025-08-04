9eea90f78bbef2179a04d17abcc962be31a21639
Samson Olofinsae <olofinsae.samson@gmail.com>
2025-08-04 20:55:56 +0100

    Clean commit: updated download_fasta.sh file

--- scripts/download_fasta.sh
+++ scripts/download_fasta.sh
@@ -9,7 +9,7 @@ FILENAME="hg19_chr8.fa"
 URL="https://www.dropbox.com/scl/fi/cl1qe1gsuzrp07tcou8h9/hg19_chr8.fa?rlkey=ibsr8j02zz7vhjf6hf1wdp4ft&st=d8mnf64n&dl=1"
 
 mkdir -p $OUTDIR
-echo "📥 Downloading $FILENAME to $OUTDIR/ ..."
+echo " Downloading $FILENAME to $OUTDIR/ ..."
 curl -L "$URL" -o "$OUTDIR/$FILENAME"
 
 echo " Download complete: $OUTDIR/$FILENAME"
